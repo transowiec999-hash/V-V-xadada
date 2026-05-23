77de1d99dae1b337cabdcbc8314a62b5c42ec7bd
transowiec999-hash <transowiec999@gmail.com>
2026-05-23 18:16:53 +0200

    Update bot.py to use hardcoded TOKEN
    Replaced TOKEN environment variable with a hardcoded token.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import asyncio
 import os
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("MTUwNzY5ODg2MDYzNjc3MDM3NA.Ge_boc.RPS8dEAjkyX_xBL7ssPP99jN0PKpvwXo5bMci0")
 
 intents = discord.Intents.default()
 intents.message_content = True
